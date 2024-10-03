0ef5f590e2ef248260c76a6a4b90ca8e4408b9c0
Matthew Webster <contactmattwebster@gmail.com>
2024-10-03 19:38:59 +0000

    edited view user profile

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from .models import User_profile
 
 
 def user_profile(request):
     """
-    Renders the About page
+    Renders the user profile page
     """
     user_profile = User_profile.objects.get(user=request.user) 
 
